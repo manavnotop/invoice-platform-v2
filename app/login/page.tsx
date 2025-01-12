3dd0956bc9989e194bdbf230e4c1f3e1a8a1b22d
manavnotop <manuagarwal1443@gmail.com>
2025-01-12 17:19:20 +0530

    add authjs w/o database adapter

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "../utils/auth";
 
 export default function Login(){
   return <>
@@ -12,7 +13,13 @@ export default function Login(){
           <CardDescription>Enter your email below to login in to your account</CardDescription>
         </CardHeader>
         <CardContent>
-          <form className="flex flex-col gap-y-4">
+          <form 
+            action={ async () => {
+              "use server";
+              await signIn();
+            }}
+            className="flex flex-col gap-y-4"
+          >
             <div className="flex flex-col gap-y-2">
               <Label>Email</Label>
               <Input placeholder="hello@hello.com" />
